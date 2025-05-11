636318ca0e403b9d85758f586aca1b0ec96bbcec
vijay <128715498+vijaykumar-143@users.noreply.github.com>
2025-05-11 07:48:48 +0530

    name.cs

---
+++ ASP.NET Core Web API/name.cs
@@ -0,0 +1,12 @@
+{
+    "Logging": {
+        "LogLevel": {
+            "Default": "Information",
+            "Microsoft.AspNetCore": "Warning"
+        }
+    },
+    "AllowedHosts": "*",
+    "ConnectionStrings": {
+        "DefaultConnection": "Server=KALKI\\SQLEXPRESS;DataBase=Tck;Trusted_Connection=true;TrustServerCertificate=true;"
+    }
+}
